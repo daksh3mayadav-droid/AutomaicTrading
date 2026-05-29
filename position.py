73e58462b255c873b4521459578730aa64fd4c3e
Daksh Yadav <daksh3mayadav@gmail.com>
2026-05-29 11:48:28 +0530

    Add position.py: empty position manager

---
+++ position.py
@@ -0,0 +1,7 @@
+class PositionManager:
+    def __init__(self, config):
+        self.positions = {}
+
+    def manage(self, trade, market_snapshot, dashboard):
+        # Demo logic placeholder
+        pass
